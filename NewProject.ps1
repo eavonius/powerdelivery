b05a5f08506a0542b12f01171818991dfb2331ce
Jayme Edwards <jayme.edwards@gmail.com>
2012-12-03 21:18:55 -0600

    Updated project generator.

--- NewProject.ps1
+++ NewProject.ps1
@@ -6,13 +6,13 @@ Param (
     [string]
     $name,
     [string]
-    $templateName = 'Blank',
+    $templateName = "Blank",
     [string]
     $tfsCollectionUri,
     [string]
     $tfsProjectName,
     [string]
-    $vsVersion = '10.0'
+    $vsVersion = "10.0"
 )
 
 Write-Host
@@ -52,15 +52,28 @@ $tfsVersionControlClientAssembly = Join-Path -Path $vsInstallDir.InstallDir -Chi
 [Reflection.Assembly]::LoadFile($tfsVersionControlClientAssembly) | Out-Null
 
 $curDir = Get-Location
+$buildsDir = Join-Path -Path $curDir -ChildPath "Builds"
+$outBaseDir = Join-Path -Path $buildsDir -ChildPath $name
+$templateDir = Join-Path -Path $curDir -ChildPath "Templates\$templateName"
 
-$outBaseDir = Join-Path -Path $curDir -ChildPath $name
-
-if (!(Test-Path -Path (Join-Path -Path $curDir -ChildPath $templateName))) {
+if (!(Test-Path -Path $templateDir)) {
     Write-Error "Template '$templateName' does not exist."
     exit
 }
 
-New-Item -ItemType Directory -Path $outBaseDir | Out-Null
-Copy-Item -Path $templateName -Destination $outBaseDir | Out-Null
+Remove-Item -Path $outBaseDir -Force -Recurse | Out-Null
+New-Item -ItemType Directory -Path $outBaseDir -Force | Out-Null
+Copy-Item -Recurse -Path "$templateDir\*" -Destination $outBaseDir -Force | Out-Null
+Move-Item -Path "$outBaseDir\Build.ps1" -Destination "$outBaseDir\$name.ps1" -Force
+Move-Item -Path "$outBaseDir\BuildLocalEnvironment.csv" -Destination "$outBaseDir\$($name)LocalEnvironment.csv" -Force
+Move-Item -Path "$outBaseDir\BuildCommitEnvironment.csv" -Destination "$outBaseDir\$($name)CommitEnvironment.csv" -Force
+Move-Item -Path "$outBaseDir\BuildTestEnvironment.csv" -Destination "$outBaseDir\$($name)TestEnvironment.csv" -Force
+Move-Item -Path "$outBaseDir\BuildCapacityTestEnvironment.csv" -Destination "$outBaseDir\$($name)CapacityTestEnvironment.csv" -Force
+Move-Item -Path "$outBaseDir\BuildProductionEnvironment.csv" -Destination "$outBaseDir\$($name)ProductionEnvironment.csv" -Force
+Copy-Item -Path (Join-Path -Path $curDir -ChildPath "PowerDeliveryEnvironment.csv") -Destination "$outBaseDir" -Force
+Copy-Item -Path (Join-Path -Path $curDir -ChildPath "BuildProcessTemplates") -Recurse -Destination "$outBaseDir" -Force
+Copy-Item -Path (Join-Path -Path $curDir -ChildPath "PowerShellModules") -Recurse -Destination "$outBaseDir" -Force
+
+Remove-Item -Path $outBaseDir -Force -Recurse | Out-Null
 
 "Done"
