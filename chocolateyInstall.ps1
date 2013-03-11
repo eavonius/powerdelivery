923b765164fb198d5b9301b850a53e4ca5135091
Jayme Edwards <jayme.edwards@gmail.com>
2013-03-11 14:41:54 -0500

    Updated chocolatey install to handle PSModulePath update correctly.

--- chocolateyInstall.ps1
+++ chocolateyInstall.ps1
@@ -1,19 +1,46 @@
 ﻿try { 
-  $powerdeliveryDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+    $powerdeliveryDir = Split-Path -parent $MyInvocation.MyCommand.Definition
   
-  Write-Host "Adding $powerdeliveryDir to PSModulePath"
+    Write-Host "Updating PSModulePath to include $powerdeliveryDir"
 
-  $psModulePath = $env:PSModulePath
+    $psModulePath = $env:PSModulePath
   
-  if (!$psModulePath.Contains($powerdeliveryDir)) {
-    [Environment]::SetEnvironmentVariable("PSModulePath", "$psModulePath;$powerdeliveryDir", "Machine")
-  }
+    $newEnvVar = $powerdeliveryDir
 
-  "Powerdelivery is now ready."
+    $caseInsensitive = [StringComparison]::InvariantCultureIgnoreCase
+
+    $pathSegment = "chocolatey\lib\powerdelivery"
+
+    if (![String]::IsNullOrWhiteSpace($psModulePath)) {
+        if (!$psModulePath.ToLower().Contains($pathSegment.ToLower())) {
+            $newEnvVar = "$psModulePath;$powerdeliveryDir"
+        }
+        else {
+            $indexOfSegment = $psModulePath.IndexOf($pathSegment, $caseInsensitive)
+            $startingSemicolon = $psModulePath.LastIndexOf(";", $indexOfSegment, $caseInsensitive)
+            $trailingSemicolon = $psModulePath.IndexOf(";", $indexOfSegment + $pathSegment.Length, $caseInsensitive)
+
+            Write-Host "$indexOfSegment $startingSemicolon $trailingSemicolon"
+
+            if ($startingSemicolon -ne -1) {
+                $psModulePrefix = $psModulePath.Substring(0, $startingSemicolon)
+                $newEnvVar = "$psModulePrefix;$powerdeliveryDir"
+                if ($trailingSemicolon -ne -1) {
+                    $newEnvVar += "$($psModulePath.Substring($trailingSemicolon))"
+                }
+            }
+        }
+    }
+
+    "Setting environment var to $newEnvVar"
+
+    [Environment]::SetEnvironmentVariable("PSModulePath", $newEnvVar, "Machine")
+
+    "Powerdelivery is now ready."
   
-  Write-ChocolateySuccess 'powerdelivery'
+    Write-ChocolateySuccess 'powerdelivery'
 } 
 catch {
-  Write-ChocolateyFailure 'powerdelivery' "$($_.Exception.Message)"
-  throw 
+    Write-ChocolateyFailure 'powerdelivery' "$($_.Exception.Message)"
+    throw 
 }
