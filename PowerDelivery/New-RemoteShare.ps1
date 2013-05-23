62eaedf17eeeb6c448f5803d00066a52cf977312
Jayme Edwards <jayme.edwards@gmail.com>
2013-05-23 13:41:23 -0700

    Add of RemoteShares Delivery Module.

---
+++ PowerDelivery/New-RemoteShare.ps1
@@ -0,0 +1,39 @@
+﻿<#
+.Synopsis
+Creates a remote UNC share.
+
+.Description
+The New-RemoteShare cmdlet will create a UNC share on the remote computer if it does not exist.
+
+.Parameter computerName
+The computer to create the share on.
+
+.Parameter shareName
+The name to give the new share.
+
+.Parameter shareDirectory
+The local path on the remote computer to share.
+
+.Parameter buildAccountName
+The account name to give full access to the share. This will allow files to be deployed by the build. 
+
+For example "MYDOMAIN\MyUser"
+
+.Example
+New-RemoteShare REMOTE_COMPUTER MyShare "C:\MyShareDir" "MYDOMAIN\MyUser"
+#>
+function New-RemoteShare {
+    [Parameter(Position=0,Mandatory=1)][string] $computerName,
+    [Parameter(Position=1,Mandatory=1)][string] $shareName, 
+    [Parameter(Position=2,Mandatory=1)][string] $shareDirectory, 
+    [Parameter(Position=3,Mandatory=1)][string] $buildAccountName
+
+    Invoke-Command -ComputerName $computerName -ScriptBlock { 
+        if (!(Test-Path -Path $shareDirectory)) {
+            New-Item $shareDirectory -ItemType Directory
+        }
+        if (!(Get-SmbShare -Name $shareName)) {
+            New-SmbShare –ContinuouslyAvailable –Name $shareName –Path $shareDirectory –FullAccess $buildAccountName
+        }
+    }
+}
