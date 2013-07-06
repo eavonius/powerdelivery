58c07bc158954d3b92a31bb02f8aac623f038c99
Jayme Edwards <jayme.edwards@gmail.com>
2013-07-06 12:17:12 -0500

    Fixed javascript.

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,6 +1,15 @@
 function setCurrentPageLinksActive() {
-	var baseUrl = location.protocol + "//" + location.host;
-	var curPage = document.URL.substring(baseUrl.length + 1);
+	var curPage = document.URL;
+	
+	var lastSeparator = curPage.lastIndexOf("/");
+	if (lastSeparator != -1) {
+		curPage = curPage.substring(lastSeparator + 1);
+	}
+	
+	var hashTag = curPage.indexOf("#");
+	if (hashTag != -1) {
+		curPage = curPage.substring(0, hashTag - 1);
+	}
 	
 	$(".nav > li > a[href='" + curPage + "']").each(function() {
 		$(this).parent().addClass('active');
