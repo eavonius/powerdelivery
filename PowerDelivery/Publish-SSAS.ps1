935fb29ac2e856f2670284b35ff42546a741849b
Jayme Edwards <jayme.edwards@gmail.com>
2014-04-22 16:46:01 -0500

    Prep for next release.

--- PowerDelivery/Publish-SSAS.ps1
+++ PowerDelivery/Publish-SSAS.ps1
@@ -3,9 +3,9 @@
 Publishes a SQL Server Analysis Services (SSAS) cube.
 
 .Description
-The Publish-SSAS cmdlet will deploy a SQL analysis services .asdatabase file to a server.
-
-Before you call the cmdlet, copy the .asdatabase from the drop location of your build to a UNC share on the SSAS server.
+The Publish-SSAS cmdlet will deploy a SQL analysis services .asdatabase file to a server. It creates a .XMLA 
+file and replaces any connection strings with updated values before copying it to a remote share on the target 
+SSAS server. After execution, a log file is available in the same directory.
 
 .Parameter asDatabase
 The .asdatabase file to deploy. Is a path local to machine specified by the computer parameter.
@@ -34,12 +34,12 @@ function Publish-SSAS {
         [Parameter(Mandatory=1)][string] $asDatabase, 
         [Parameter(Mandatory=1)][string] $computer, 
         [Parameter(Mandatory=0)][string] $sqlVersion = '11.0',
-		[Parameter(Mandatory=0)][string] $deploymentUtilityPath = "C:\Program Files (x86)\Microsoft SQL Server\110\Tools\Binn\ManagementStudio\Microsoft.AnalysisServices.Deployment.exe",
-		[Parameter(Mandatory=0)][string] $cubeName,
+        [Parameter(Mandatory=0)][string] $deploymentUtilityPath = "C:\Program Files (x86)\Microsoft SQL Server\110\Tools\Binn\ManagementStudio\Microsoft.AnalysisServices.Deployment.exe",
+        [Parameter(Mandatory=0)][string] $cubeName,
         [Parameter(Mandatory=0)] $connections
     )
-	
-	Set-Location $powerdelivery.deployDir
+    
+    Set-Location $powerdelivery.deployDir
 
     $logPrefix = "Publish-SSAS:"
 
@@ -57,18 +57,18 @@ function Publish-SSAS {
 
     Start-Sleep -Seconds 15
 
-    $xmlaPath = Join-Path -Path (Get-Location) -ChildPath $xmlaPath
+    $xmlaFullPath = Join-Path -Path (Get-Location) -ChildPath $xmlaPath
 
     $computerNames = $computer -split "," | % { $_.Trim() }
 
     foreach ($curComputerName in $computerNames) {
-    	
-    	$newModelName = $asModelName
+        
+        $newModelName = $asModelName
 
-    	if (![String]::IsNullOrWhiteSpace($cubeName)) {
+        if (![String]::IsNullOrWhiteSpace($cubeName)) {
 
-    		$newModelName = $cubeName
-		    [xml]$xmlaDoc = Get-Content $xmlaPath
+            $newModelName = $cubeName
+            [xml]$xmlaDoc = Get-Content $xmlaFullPath
 
             $ns = new-object Xml.XmlNamespaceManager $xmlaDoc.NameTable
             $ns.AddNamespace('xmla', 'http://schemas.microsoft.com/analysisservices/2003/engine')
@@ -76,30 +76,74 @@ function Publish-SSAS {
             $objectNode = $xmlaDoc.SelectSingleNode("//xmla:Batch/xmla:Alter/xmla:Object", $ns)
             $objectNode.DatabaseID = $cubeName
 
-		    $databaseNode = $xmlaDoc.SelectSingleNode("//xmla:Batch/xmla:Alter/xmla:ObjectDefinition/xmla:Database", $ns)
-			$databaseNode.ID = $cubeName
-			$databaseNode.Name = $cubeName
-		    
+            $databaseNode = $xmlaDoc.SelectSingleNode("//xmla:Batch/xmla:Alter/xmla:ObjectDefinition/xmla:Database", $ns)
+            $databaseNode.ID = $cubeName
+            $databaseNode.Name = $cubeName
+            
             $objectToProcessNode = $xmlaDoc.SelectSingleNode("//xmla:Batch/xmla:Parallel/xmla:Process/xmla:Object", $ns)
             $objectToProcessNode.DatabaseID = $cubeName
 
-		    $xmlaDoc.Save($xmlaPath)
+            if ($connections -ne $null) {
+        
+                $connections.Keys | % {
+                    $connection = $connections[$_]
+
+                    $connectionStringNode = $databaseNode.SelectSingleNode("xmla:DataSources/xmla:DataSource[xmla:Name='$($connection.ConnectionName)']/xmla:ConnectionString", $ns)
+
+                    if ($connectionStringNode -eq $null) {
+                        throw "Unable to find connection named '$($connection.ConnectionName)' connection to update."
+                    }
+
+                    $connectionStringNode.'#text' = $connection.ConnectionString
+                }
+            }
+
+            $xmlaDoc.Save($xmlaFullPath)
         }
 
-        Invoke-ASCMD -server "$curComputerName" -inputFile "$xmlaPath"
+        Deploy-BuildAssets -computerName $curComputerName -path $asFilesDir -destination $asFilesDir
 
-    	Write-BuildSummaryMessage -name "Deploy" -header "Deployments" -message "SSAS: $($asModelName).asdatabase -> $newModelName ($curComputerName)"
+        $localDeployPath = Get-ComputerLocalDeployPath $curComputerName
+        $remoteDeployPath = Get-ComputerRemoteDeployPath $curComputerName
 
-        if ($connections -ne $null) {
-        
-            $connections.Keys | % {
-    			$connection = $connections[$_]
-    			Set-SSASConnection -computer $curComputerName `
-    						       -databaseName $cubeName `
-    						       -connectionName $connection.ConnectionName `
-    						       -datasourceID $connection.DataSourceID `
-    						       -connectionString $connection.ConnectionString
-    		}
+        $xmlaLocalDeployPath = [System.IO.Path]::Combine($localDeployPath, $xmlaPath)
+        $outLocalDeployPath = $xmlaLocalDeployPath -replace ".xmla", "ExecutionLog.xml"
+
+        $remoteCommand = "Invoke-ASCMD -server ""$curComputerName"" -inputFile ""$xmlaLocalDeployPath"" | Out-File ""$outLocalDeployPath"""
+
+        "$logPrefix $remoteCommand"
+
+        Invoke-Expression "Invoke-Command -ComputerName ""$curComputerName"" -ScriptBlock { $remoteCommand }"
+
+        $outRemoteDeployPath = Join-Path $remoteDeployPath ($xmlaPath -replace ".xmla", "ExecutionLog.xml")
+
+        [xml]$outDoc = Get-Content $outRemoteDeployPath
+
+        $nsOut = new-object Xml.XmlNamespaceManager $outDoc.NameTable
+        $nsOut.AddNamespace('xa', 'urn:schemas-microsoft-com:xml-analysis')
+        $nsOut.AddNamespace('xae', 'urn:schemas-microsoft-com:xml-analysis:empty')
+        $nsOut.AddNamespace('xex', 'urn:schemas-microsoft-com:xml-analysis:exception')
+
+        $warningNodes = $outDoc.SelectNodes("//xa:return/xae:root/xex:Messages/xex:Warning", $nsOut)
+
+        $deploySuccess = $true
+
+        if ($warningNodes -ne $null) {
+            foreach ($warningNode in $warningNodes) {
+                
+                $warningDesc = $warningNode.Description
+                
+                "SSAS Deployment Error: $warningDesc"
+
+                $deploySuccess = $false
+            }
+        }
+
+        if ($deploySuccess) {
+            Write-BuildSummaryMessage -name "Deploy" -header "Deployments" -message "SSAS: $($asModelName).asdatabase -> $newModelName ($curComputerName)"
+        }
+        else {
+            throw "One or more errors occurred deploying an SSAS cube. See the build log for details."
         }
     }
 }
