095a36b92ec20b73dc4b29e3aca1de413d4bc06f
Jayme Edwards <jayme.edwards@gmail.com>
2014-09-02 17:04:08 -0500

    More fixes to installers

--- VisualStudioExtension/PowerDeliveryVSExtension2012/Properties/AssemblyInfo.cs
+++ VisualStudioExtension/PowerDeliveryVSExtension2012/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Security.Permissions;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.6")]
-[assembly: AssemblyFileVersion("1.0.6")]
+[assembly: AssemblyVersion("1.0.7")]
+[assembly: AssemblyFileVersion("1.0.7")]
