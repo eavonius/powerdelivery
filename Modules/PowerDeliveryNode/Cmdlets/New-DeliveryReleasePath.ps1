e5c0d20786e16dee83ed2ad133eb4a8523f17b3d
Jayme Edwards <jayme.edwards@gmail.com>
2015-10-24 15:21:26 -0500

    Initial add of release path cmdlets

--- Modules/PowerDeliveryNode/Cmdlets/New-DeliveryReleasePath.ps1
+++ Modules/PowerDeliveryNode/Cmdlets/New-DeliveryReleasePath.ps1
@@ -1,5 +1,89 @@
-function New-DeliveryReleasePath {
+<#
+.Synopsis
+Creates a new release directory on a remote node being deployed to with powerdelivery.
 
+.Description
+Creates a new release directory on a remote node being deployed to with powerdelivery. 
+A directory will be created within the path you specify and named after the ProjectName 
+property of the target. A directory will be created within it with the timestamp of the 
+current run of the target and symbolicly linked to "Current".
+
+Any releases older than the number in the Keep parameter will be deleted.
+
+.Example
+Delivery:Role -Up {
+  param($target, $config, $node)
+
+  # You must install PowerDeliveryNode using chocolatey 
+  # or something else on the remote node first.
+  Import-Module PowerDeliveryNode
+
+  # $releasePath will be C:\Users\<User>\AppData\Roaming\<Project>\Current 
+  # pointing to a yyyyMMdd_hhmmss folder in the same directory.
+  $releasePath = New-DeliveryReleasePath $target [Environment]::GetFolderPath("AppData")
+} -Down {
+  
+  # This will rollback a previous release. If no previous 
+  # release exists it will be the same path as current.
+  $releasePath = Undo-DeliveryReleasePath $target [Environment]::GetFolderPath("AppData")
+}
+
+The release will be created at the path:
+
+C:\Users\<User>\AppData\Roaming\<Project>\<StartedAt>
+
+And symlinked to:
+
+C:\Users\<User>\AppData\Roaming\<Project>\Current
+
+.Parameter Target
+The hash of target properties for the powerdelivery run.
+
+.Parameter Path
+The parent path into which to create the release path.
+
+.Parameter Keep
+The number of previous releases to keep. Defaults to 5.
+#>
+function New-DeliveryReleasePath {
+  [CmdletBinding()]
+  param(
+    [Parameter(Position=0,Mandatory=1)][hashtable] $Target,
+    [Parameter(Position=1,Mandatory=1)][string] $Path,
+    [Parameter(Position=2,Mandatory=0)][int] $Keep = 5
+  )
+
+  # Reference a sub-directory named after the project
+  $projectPath = Join-Path $Path $target.ProjectName
+
+  # Create a directory for this release
+  $thisReleasePath = Join-Path $projectPath $target.StartedAt
+  if (!(Test-Path $thisReleasePath)) {
+    New-Item $thisReleasePath -ItemType Directory | Out-Null
+  }
+
+  # Remove old link to current release
+  $currentReleasePath = Join-Path $projectPath "Current"
+  if (Test-Path $currentReleasePath) {
+    & cmd /c "rmdir ""$currentReleasePath"""
+  }
+
+  # Link this release to the current release
+  & cmd /c "mklink /J ""$currentReleasePath"" ""$thisReleasePath""" | Out-Null
+
+  # Get releases
+  $releases = Get-ChildItem -Directory $projectPath -Exclude "Current"
+
+  # Delete releases older than the last 5
+  if ($releases.count -gt $Keep) {
+    $oldReleaseCount = $releases.count - $Keep
+    $releases | 
+      Sort-Object -Property Name | 
+        Select -First $oldReleaseCount | 
+          Remove-Item -Force -Recurse | Out-Null
+  }
+
+  $currentReleasePath
 }
 
 Export-ModuleMember -Function New-DeliveryReleasePath
---
+++ Modules/PowerDeliveryNode/Cmdlets/Undo-DeliveryReleasePath.ps1
@@ -0,0 +1,88 @@
+<#
+.Synopsis
+Rolls back to a previous release directory on a remote node being deployed to with powerdelivery.
+
+.Description
+Rolls back to a previous release directory on a remote node being deployed to with powerdelivery.
+Modifies the symbolic link pointing to the current release path to point to the previous release 
+and deletes the old current release directory. If no previous release exists, will leave the 
+current release as is and return it.
+
+.Example
+Delivery:Role -Up {
+  param($target, $config, $node)
+
+  # You must install PowerDeliveryNode using chocolatey 
+  # or something else on the remote node first.
+  Import-Module PowerDeliveryNode
+
+  # $releasePath will be C:\Users\<User>\AppData\Roaming\<Project>\Current
+  $releasePath = New-DeliveryReleasePath $target [Environment]::GetFolderPath("AppData")
+} -Down {
+  
+  # This will rollback a previous release. If no previous 
+  # release exists it will be the same path as current.
+  $releasePath = Undo-DeliveryReleasePath $target [Environment]::GetFolderPath("AppData")
+}
+
+The releases will be looked for in the path:
+
+C:\Users\<User>\AppData\Roaming\<Project>
+
+And the previous release symlinked to:
+
+C:\Users\<User>\AppData\Roaming\<Project>\Current
+
+.Parameter Target
+The hash of target properties for the powerdelivery run.
+
+.Parameter Path
+The parent path in which to look for releases.
+#>
+function Undo-DeliveryReleasePath {
+  [CmdletBinding()]
+  param(
+    [Parameter(Position=0,Mandatory=1)][hashtable] $Target,
+    [Parameter(Position=1,Mandatory=1)][string] $Path
+  )
+
+  $previousReleasePath = $null
+
+  # Reference a sub-directory named after the project
+  $projectPath = Join-Path $Path $target.ProjectName
+
+  # If at least one release has occurred
+  if (Test-Path $projectPath) {
+
+    # Get current and previous release
+    $lastRelease = Get-ChildItem -Directory $projectPath -Exclude "Current" | 
+      Sort-Object -Descending -Property Name | Select -First 2
+
+    # Only rollback if we've got a previous release
+    if ($lastRelease.count -eq 2) {
+
+      $previousReleasePath = $lastRelease[1]
+
+      # Remove link to current release
+      $currentReleasePath = Join-Path $projectPath "Current"
+      if (Test-Path $currentReleasePath) {
+        & cmd /c "rmdir ""$currentReleasePath"""
+      }
+
+      # Link current to previous release
+      & cmd /c "mklink /J ""$currentReleasePath"" ""$previousReleasePath""" | Out-Null
+
+      # Delete old current release
+      Remove-Item -Force -Recurse $lastRelease[0] | Out-Null
+    }
+    elseif ($lastRelease.count -eq 1) {
+
+      # Return current if no previous release
+      $previousReleasePath = Join-Path $projectPath "Current"
+    }
+  }
+
+  $previousReleasePath
+}
+
+Export-ModuleMember -Function Undo-DeliveryReleasePath
