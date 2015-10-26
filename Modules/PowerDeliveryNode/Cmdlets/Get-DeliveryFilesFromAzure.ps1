bef4d9ac6fd8a11cf4e4eeaf102592dfd14a0ace
Jayme Edwards <jayme.edwards@gmail.com>
2015-10-26 16:12:28 -0500

    Updated with debug lines

--- Modules/PowerDeliveryNode/Cmdlets/Get-DeliveryFilesFromAzure.ps1
+++ Modules/PowerDeliveryNode/Cmdlets/Get-DeliveryFilesFromAzure.ps1
@@ -136,9 +136,20 @@ function Get-DeliveryFilesFromAzure {
 
       # Fix up the filename to exclude the release and timestamp
       $targetPath = $releaseFile.Name.Substring($releasePrefix.Length + 1)
+
+      Write-Host "Target Path: $targetPath"
+
       $targetPath = $targetPath -replace '/', '\'
+
+      Write-Host "Target Path Replaced: $targetPath"
+
       $targetPath = Join-Path $Destination $targetPath
+
+      Write-Host "Target Path Appended: $targetPath"
+
       $targetDir = [IO.Path]::GetDirectoryName($targetPath)
+
+      Write-Host "Target Dir: $targetDir"
 
       # Create the directory containing the file if it doesn't exist
       if (!(Test-Path $targetDir)) {
